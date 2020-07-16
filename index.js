100ee63c5e0f7d3bfc0a7b4ee165e4a135b54b77
qhdgkdbs <qhdgkdbs@gmail.com>
2020-07-16 16:22:53 +0900

    db스케마 설정

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://qhdgkdbs:<password>@boil.slyxt.mongodb.net/<dbname>?retryWrites=true&w=majority', { 
+mongoose.connect('mongodb+srv://qhdgkdbs:@tmddn851@boil.slyxt.mongodb.net/<dbname>?retryWrites=true&w=majority', { 
     useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
-})
+}).then(() => console.log("connected"))
+.catch(err => console.log(err))
 
 
 
